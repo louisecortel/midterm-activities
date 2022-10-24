46f9201a18fa3bbf1496d9cdf9f8b66d9fe59515
louisecortel <116581062+louisecortel@users.noreply.github.com>
2022-10-25 00:02:06 +0800

    Add files via upload

---
+++ cortel-oop-RunBloodData.java
@@ -0,0 +1,33 @@
+import java.util.Scanner;
+
+public class RunBloodData{
+   
+    
+    public static void main (String [] args){
+    Scanner sc = new Scanner(System.in);    
+    BloodData bds = new BloodData();
+        
+    System.out.println("Enter blood type of patient");
+    String input1 = sc.nextLine();
+    System.out.println("Enter the Rhesus factor(+ or -):");
+    String input2 = sc.nextLine();
+    bds.display();
+    
+    if(input1.isEmpty()){
+        if (input2.isEmpty)){
+    System.out.println("");
+    BloodData bd = new BloodData();
+    }
+        
+        else
+        BloodData bd = newBloodData();
+        Sysdtem.out.println("Invalid")
+                
+                
+    
+       
+   
+    
+    }
+}
+
---
+++ cortel-oop-blooddata.java
@@ -0,0 +1,25 @@
+class BloodData{
+    
+static String bloodType;
+static String rhFactor;
+
+public BloodData(){
+    bloodType = "O"
+    rhFactor = "+"
+}
+
+public BloodData (String bt, String rh){
+    bloodType = bt;
+    rhFactor = rh;
+    
+public void display(){
+System.out.println( bt + rh + "is added to the blood bank. \n");
+
+   }
+    
+
+}
+    
+
+
+
